4193696fd4d5d89bb957ed5772a3870958bf1f38
pashok <122551919+pashok11211@users.noreply.github.com>
2024-09-16 18:20:57 +0300

    исправлены ошибки в  purebred_dog.py

--- purebred_dog.py
+++ purebred_dog.py
@@ -1,15 +1,16 @@
 from dog import Dog
 
+
 class PurebredDog(Dog):
-    def __init__(self, name, age, color, breed):
-        super().__init__(name, age, color)
+    def __init__(self, name2, age2, color2, breed):
+        super().__init__(name2, age2, color2)
         self._breed = breed
 
     def breed(self):
         return self._breed
 
     def bark(self):
-        return super().bark()
+        return f"Собака {self.name()}, возвраст {self.age()}, цвет {self.color} гавкает громко."
 
     def go_to_dog_show(self):
-        return f"Собака {self.name}, породы {self.breed} участвует в выставке."
+        return f"Собака {self.name()}, породы {self.breed()} участвует в выставке."
