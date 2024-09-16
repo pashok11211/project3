76bfd57d3fbf144737676354b1704c5b472131eb
pashok <122551919+pashok11211@users.noreply.github.com>
2024-09-16 18:20:03 +0300

    исправлены ошибки в dog.py

--- dog.py
+++ dog.py
@@ -2,7 +2,7 @@ class Dog:
     def __init__(self, name, age, color):
         self._name = name
         self._age = age
-        self._color = color
+        self.color = color
 
     def name(self):
         return self._name
@@ -10,11 +10,5 @@ class Dog:
     def age(self):
         return self._age
 
-    def color(self):
-        return self._color
-
-    def age(self, value):
-        self._age = value
-
     def bark(self):
-        return f"Собака {self.name}, возраста {self.age}, цвета {self.color} гавкает громко"
+        return f"Собака {self.name()}, возраст {self.age}, цвет {self.color} гавкает громко."
